66b28d17c645387ade9360051f8536c4f8b2f003
rajibtalukder <rajibtalukder392@gmail.com>
2026-05-25 15:23:09 +0600

    Habit model added now

--- Habit Tracker/ContentView.swift
+++ Habit Tracker/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  Habit Tracker
-//
-//  Created by YOTECH MAC01 on 25/05/2026.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -17,8 +10,4 @@ struct ContentView: View {
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
